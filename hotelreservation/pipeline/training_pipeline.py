d5113ba70a4ac07cc1390b3b0141f210564339ed
BenGJ10 <bengj1015@gmail.com>
2025-09-02 00:42:36 +0530

    Added training pipeline

---
+++ hotelreservation/pipeline/training_pipeline.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from config.config_entities import *
+from hotelreservation.utils.main_utils import read_yaml_file
+from hotelreservation.logger.logger import logging
+from hotelreservation.exception.exception import CustomException
+from hotelreservation.components.data_ingestion import DataIngestion
+from hotelreservation.components.data_processing import DataProcessor
+from hotelreservation.components.model_training import ModelTraining
+
+
+if __name__ == "__main__":
+
+    try:
+        logging.info("Starting the entire training pipeline...")
+        
+        # Initiating Data Ingestion
+        
+        data_ingestion = DataIngestion(read_yaml_file(CONFIG_PATH))
+        data_ingestion.initiate_data_ingestion()
+
+        # Initiating Data Processing
+        
+        data_processor = DataProcessor(TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR, CONFIG_PATH)
+        data_processor.initiate_data_processing()
+        
+        # Initiating Model Training
+
+        model_trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH, PROCESSED_TEST_DATA_PATH, MODEL_PATH)
+        model_trainer.initiate_model_training()
+
+    except Exception as e:
+        raise CustomException(e, sys)
