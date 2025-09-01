3a6163c4ff56afe8434f073f26631037a4018870
BenGJ10 <bengj1015@gmail.com>
2025-09-01 17:03:08 +0530

    Added Data Processing entities

--- config/config_entities.py
+++ config/config_entities.py
@@ -8,5 +8,13 @@ RAW_DIR = "artifacts/data_ingestion"
 RAW_FILE_PATH = os.path.join(RAW_DIR, "raw.csv")
 TRAIN_FILE_PATH = os.path.join(RAW_DIR, "train.csv")
 TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
-
 CONFIG_PATH = "config/config.yaml"
+
+
+"""
+Data Processing related paths configuration
+"""
+PROCESSED_DIR = "artifacts/data_processed"
+PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR, "processed_train.csv")
+PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR, "processed_test.csv")
+TARGET_COLUMN = 'booking_status'
